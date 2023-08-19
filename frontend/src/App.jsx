fd442c8950a6d8960f74a5dfc04614c2df773925
isuruij <2020uelog@gmail.com>
2023-08-19 20:45:37 +0530

    styled navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,20 +25,27 @@ function App() {
         setnewsList(res.data);
       }
     }
-    getData(); 
+    getData();
   }, [keyword]);
 
   return (
     <div class="back">
       {/* start nav bar code */}
       <div style={{ position: "fixed", width: "100%", top: "0", zIndex: "10" }}>
-        <nav className="navbar navbar-expand-lg bg-primary navbar-dark">
+        <nav className="navbar navbar-expand-lg bg-primary navbar-dark ">
           <div className="container-fluid">
             <a className="navbar-brand" href="http://localhost:5173/">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-newspaper" viewBox="0 0 16 16">
-  <path d="M0 2.5A1.5 1.5 0 0 1 1.5 1h11A1.5 1.5 0 0 1 14 2.5v10.528c0 .3-.05.654-.238.972h.738a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 1 1 0v9a1.5 1.5 0 0 1-1.5 1.5H1.497A1.497 1.497 0 0 1 0 13.5v-11zM12 14c.37 0 .654-.211.853-.441.092-.106.147-.279.147-.531V2.5a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0-.5.5v11c0 .278.223.5.497.5H12z"/>
-  <path d="M2 3h10v2H2V3zm0 3h4v3H2V6zm0 4h4v1H2v-1zm0 2h4v1H2v-1zm5-6h2v1H7V6zm3 0h2v1h-2V6zM7 8h2v1H7V8zm3 0h2v1h-2V8zm-3 2h2v1H7v-1zm3 0h2v1h-2v-1zm-3 2h2v1H7v-1zm3 0h2v1h-2v-1z"/>
-</svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-newspaper"
+                viewBox="0 0 16 16"
+              >
+                <path d="M0 2.5A1.5 1.5 0 0 1 1.5 1h11A1.5 1.5 0 0 1 14 2.5v10.528c0 .3-.05.654-.238.972h.738a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 1 1 0v9a1.5 1.5 0 0 1-1.5 1.5H1.497A1.497 1.497 0 0 1 0 13.5v-11zM12 14c.37 0 .654-.211.853-.441.092-.106.147-.279.147-.531V2.5a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0-.5.5v11c0 .278.223.5.497.5H12z" />
+                <path d="M2 3h10v2H2V3zm0 3h4v3H2V6zm0 4h4v1H2v-1zm0 2h4v1H2v-1zm5-6h2v1H7V6zm3 0h2v1h-2V6zM7 8h2v1H7V8zm3 0h2v1h-2V8zm-3 2h2v1H7v-1zm3 0h2v1h-2v-1zm-3 2h2v1H7v-1zm3 0h2v1h-2v-1z" />
+              </svg>
             </a>
             <button
               className="navbar-toggler"
@@ -77,45 +84,91 @@ function App() {
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#" onClick={()=>{
-                    setkeyword("Cyberattack")
-                  }}>
+                  <a
+                    className="nav-link"
+                    href="#"
+                    onClick={() => {
+                      setkeyword("Cyberattack");
+                    }}
+                  >
                     Cyberattack
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link " href="#" onClick={()=>{
-                    setkeyword("UFO sighting")
-                  }}>
+                  <a
+                    className="nav-link "
+                    href="#"
+                    onClick={() => {
+                      setkeyword("UFO sighting");
+                    }}
+                  >
                     UFO sighting
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#" onClick={()=>{
-                    setkeyword("Natural disaster")
-                  }}>
+                  <a
+                    className="nav-link"
+                    href="#"
+                    onClick={() => {
+                      setkeyword("Natural disaster");
+                    }}
+                  >
                     Natural disaster
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#" onClick={()=>{
-                    setkeyword("Tech innovation")
-                  }}>
+                  <a
+                    className="nav-link"
+                    href="#"
+                    onClick={() => {
+                      setkeyword("Tech innovation");
+                    }}
+                  >
                     Tech innovation
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#" onClick={()=>{
-                    setkeyword("Space Exploration")
-                  }}>
+                  <a
+                    className="nav-link"
+                    href="#"
+                    onClick={() => {
+                      setkeyword("Space Exploration");
+                    }}
+                  >
                     Space Exploration
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#" onClick={()=>{
-                    setkeyword("Economy")
-                  }}>
+                  <a
+                    className="nav-link"
+                    href="#"
+                    onClick={() => {
+                      setkeyword("Economy");
+                    }}
+                  >
                     Economy
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link"
+                    href="#"
+                    onClick={() => {
+                      setkeyword("Artificial Intelligence");
+                    }}
+                  >
+                    Artificial Intelligence
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link"
+                    href="#"
+                    onClick={() => {
+                      setkeyword("Sports");
+                    }}
+                  >
+                    Sports
                   </a>
                 </li>
               </ul>
